8ddb7698038cf482ad5994f1e16787c27f1a1f19
Mukenzii <mcmuhi13@gmail.com>
2024-10-18 02:17:19 +0500

    Added audio

--- bot.py
+++ bot.py
@@ -23,6 +23,69 @@ def reaction_start(message: Message):
 
     bot.send_message(chat_id, 'Salom', reply_markup=buttons())
 
+@bot.message_handler(commands=['video'])
+def command_video(message: Message):
+    group_id = -4129582016
+    bot.send_message(group_id,"Url kiriting")
+    bot.register_next_step_handler(message,group_download)
+
+@bot.message_handler(commands=['audio'])
+def command_audio(message: Message):
+    group_id = -4129582016
+    bot.send_message(group_id,"Url kiriting")
+    bot.register_next_step_handler(message,group_audio)
+
+def group_download(message: Message):
+    url = message.text
+    group_id = -4129582016
+
+    try:
+        video = YouTube(url)
+        download = video.streams.get_highest_resolution()
+        vid = download.download()
+
+        with open(vid, 'rb') as video_to_send:
+            try:
+                bot.send_message(group_id, "Video yuklab olinmoqda...")
+                bot.send_video(group_id, video_to_send)
+                bot.send_message(group_id, "Video muvaffaqiyatli yuklab olindi", reply_markup=buttons())
+
+            except:
+                bot.send_message(group_id, "File hajmi 50mbdan katta bo'lmasligi kerak")
+        os.remove(vid)
+
+
+
+    except:
+        bot.send_message(group_id, "Urlni to'g'ri kiriting")
+
+def group_audio(message: Message):
+    url = message.text
+    group_id = -4129582016
+
+    try:
+        audio = YouTube(url)
+        download = audio.streams.get_audio_only()
+        aud = download.download()
+
+        with open(aud, 'rb') as audio_to_send:
+            try:
+                bot.send_message(group_id, "Audio yuklab olinmoqda...")
+                bot.send_audio(group_id, audio_to_send)
+                bot.send_message(group_id, "Audio muvaffaqiyatli yuklab olindi", reply_markup=buttons())
+
+            except:
+                bot.send_message(group_id, "File hajmi 50mbdan katta bo'lmasligi kerak")
+        os.remove(aud)
+
+
+
+    except:
+        bot.send_message(group_id, "Urlni to'g'ri kiriting")
+
+
+
+
 
 @bot.message_handler(func=lambda message: message.text == 'Video')
 def reaction_video(message: Message):
